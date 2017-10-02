535f26a2ae0c4511da4b00ed46511bf2f547c952
Allan James Lapid <ajLapid718@gmail.com>
2017-10-02 14:38:32 -0400

    Day One: Get user input && use the plus sign for addition and concatenation

--- Day-01-Data-Types.rb
+++ Day-01-Data-Types.rb
@@ -1,15 +1,15 @@
 # Task
 # Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
-#
+
 # Declare  variables: one of type int, one of type double, and one of type String.
 # Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
-# Use the  operator to perform the following operations: 
+# Use the  operator to perform the following operations:
 # Print the sum of  plus your int variable on a new line.
 # Print the sum of  plus your double variable to a scale of one decimal place on a new line.
 # Concatenate  with the string you read as input and print the result on a new line.
 
 # Explanation
-#
+
 # When we sum the integers  and , we get the integer .
 # When we sum the floating-point numbers  and , we get .
 # When we concatenate HackerRank with is the best place to learn and practice coding!, we get HackerRank is the best place to learn and practice coding!.
