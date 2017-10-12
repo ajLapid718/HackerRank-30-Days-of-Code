7591472cbf5add8715d8ef533d846972d7b158b3
Allan James Lapid <ajLapid718@gmail.com>
2017-10-11 21:15:22 -0400

    Return the max_consecutive_ones within the binary representation of a number

---
+++ Day-10-Binary_Numbers.rb
@@ -0,0 +1,28 @@
+# Task
+# Given a base-10 integer, n, convert it to binary (base-2). Then find and print the base-10 integer denoting the maximum number of consecutive 1's in n's binary representation.
+
+# Explanation
+
+# Sample Case 1:
+# The binary representation of 5 is 101, so the maximum number of consecutive 1's is 1.
+
+# Sample Case 2:
+# The binary representation of 13 is 1101, so the maximum number of consecutive 1's is 2.
+
+# My solution
+# I am pretty satisfied with incrementing a counter variable but I need to learn more about the nature of binary numbers so that I can solve the problem without calling #to_s(2)
+n = gets.strip.to_i
+
+most_so_far = 0
+running_tally = 0
+
+n.to_s(2).each_char do |char|
+  if char == "1"
+    running_tally += 1
+    most_so_far = running_tally if running_tally > most_so_far
+  else
+    running_tally = 0
+  end
+end
+
+print most_so_far
