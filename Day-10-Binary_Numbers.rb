66bd2bafb9c09e763497a334fda214704d97d142
Allan James Lapid <ajLapid718@gmail.com>
2017-10-12 05:06:14 -0400

    Day Ten: Go from base-10 to base-2 & count consecutive 1s

--- Day-10-Binary_Numbers.rb
+++ Day-10-Binary_Numbers.rb
@@ -11,12 +11,39 @@
 
 # My solution
 # I am pretty satisfied with incrementing a counter variable but I need to learn more about the nature of binary numbers so that I can solve the problem without calling #to_s(2)
+
 n = gets.strip.to_i
 
 most_so_far = 0
 running_tally = 0
 
 n.to_s(2).each_char do |char|
+  if char == "1"
+    running_tally += 1
+    most_so_far = running_tally if running_tally > most_so_far
+  else
+    running_tally = 0
+  end
+end
+
+print most_so_far
+
+# A less Ruby-esque, more general way of solving the problem (particularly converting bases)
+
+n = gets.strip.to_i
+
+arr = []
+
+until n == 0
+  remainder = (n % 2)
+  arr.unshift(remainder)
+  n /= 2
+end
+
+running_tally = 0
+most_so_far = 0
+
+arr.join.chars.each do |char|
   if char == "1"
     running_tally += 1
     most_so_far = running_tally if running_tally > most_so_far
