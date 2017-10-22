54b580b189bb865cc9085e48f953e96bf1137f81
Allan James Lapid <ajLapid718@gmail.com>
2017-10-21 21:56:29 -0400

    Day Twenty: Track the amount of swaps of a bubble sort

---
+++ Day-20-Sorting.rb
@@ -0,0 +1,44 @@
+Task
+
+Given an array, a, of size n distinct elements, sort the array in ascending order using the Bubble Sort algorithm.
+Once sorted, print the following 3 lines:
+
+"Array is sorted in numSwaps swaps." || where numSwaps is the number of swaps that took place.
+"First Element: firstElement" || where firstElement is the first element in the sorted array.
+"Last Element: lastElement" || where lastElement is the last element in the sorted array.
+
+Sample Input
+
+3
+1 2 3
+
+Sample Output 0
+
+Array is sorted in 0 swaps.
+First Element: 1
+Last Element: 3
+
+# Provided by HackerRank
+n = gets.strip.to_i
+a = gets.strip
+a = a.split(' ').map(&:to_i)
+
+# My solution
+counter = 0
+loop do
+    sorted = true
+
+    a[0...-1].each_index do |i|
+        if a[i] > a[i+1]
+            sorted = false
+            counter += 1
+            a[i], a[i+1] = a[i+1], a[i]
+        end
+    end
+
+    break if sorted == true
+end
+
+puts "Array is sorted in #{counter} swaps."
+puts "First Element: #{a.first}"
+puts "Last Element: #{a.last}"
